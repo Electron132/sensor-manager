ba684e86a9d8a64b5f54662e5560365d80ed69bf
happy <2524287770@qq.com>
2026-07-16 15:02:17 +0800

    阶段 2：保存和展示多个传感器

--- src/main.cpp
+++ src/main.cpp
@@ -61,13 +61,54 @@ class m_Sensor {
 };
 
 int main() {
-    struct Sensor sensor1={1,"DS18B20",
-        SensorType::Temperature,0,"C",
-    1,""};
+    struct Sensor sensor1={
+        1,
+        "DS18B20",
+        SensorType::Temperature,
+        0,
+        "C",
+        1,
+        ""
+    };
+    //m_Sensor m_sensor1(sensor1);
 
-    m_Sensor sensorManage(sensor1);
+    struct Sensor sensor2={
+        2,
+        "DS18B21",
+        SensorType::Humidity,
+        0,
+        "RH",
+        0,
+        ""
+    };
+    //m_Sensor m_sensor2(sensor2);
 
-    sensorManage.printInfo();
+    struct Sensor sensor3={
+        3,
+        "DS18B23",
+        SensorType::Light,
+        0,
+        "cd",
+        1,
+        ""
+    };
+    //m_Sensor m_sensor3(sensor3);
+
+
+   //std::vector<m_Sensor> sensors={m_sensor1,m_sensor2,m_sensor3};
+    std::vector<m_Sensor> sensors;
+    sensors.reserve(3); // 预分配内存，防止扩容
+    sensors.emplace_back(sensor1);
+    sensors.emplace_back(sensor2);
+    sensors.emplace_back(sensor3);
+
+    if (sensors.empty()) {
+        std::cout << "No sensors available.\n";
+    } else {
+        for (const auto& sensor : sensors) {
+            sensor.printInfo();
+        }
+    }
 
 
 }
